dd708844c44891e39a48140b66a1cbe6351b9976
jkanning@gmail.com <jkanning@gmail.com>
2019-01-13 20:37:04 -0500

    Adding the rest of the project files.

---
+++ SweetwaterTest/SweetwaterComments.aspx
@@ -0,0 +1,54 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="SweetwaterComments.aspx.cs" Inherits="SweetwaterTest.SweetwaterComments" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <p>
+			Candy:</p>
+		<p>
+			<asp:TextBox ID="txtCandy" runat="server" Height="100px" TextMode="MultiLine" Width="950px"></asp:TextBox>
+		</p>
+		<p>
+			&nbsp;</p>
+		<p>
+			Call Me / Don&#39;t Call Me:</p>
+		<p>
+			<asp:TextBox ID="txtCallMe" runat="server" Height="100px" TextMode="MultiLine" Width="950px"></asp:TextBox>
+		</p>
+		<p>
+			&nbsp;</p>
+		<p>
+			Referred:</p>
+		<p>
+			<asp:TextBox ID="txtReferred" runat="server" Height="100px" TextMode="MultiLine" Width="950px"></asp:TextBox>
+		</p>
+		<p>
+			&nbsp;</p>
+		<p>
+			Signature:</p>
+		<p>
+			<asp:TextBox ID="txtSignature" runat="server" Height="100px" TextMode="MultiLine" Width="950px"></asp:TextBox>
+		</p>
+		<p>
+			&nbsp;</p>
+		<p>
+			<asp:Label ID="Label1" runat="server" Text="Display Comments"></asp:Label>
+		</p>
+		<p>
+			<asp:TextBox ID="txtDisplay" runat="server" Height="221px" TextMode="MultiLine" Width="950px"></asp:TextBox>
+		</p>
+		<p>
+			&nbsp;</p>
+		<p>
+			<asp:TextBox ID="txtTest" runat="server" Height="236px" TextMode="MultiLine" Visible="False" Width="1080px" Wrap="False"></asp:TextBox>
+		</p>
+        <div>
+        </div>
+    </form>
+</body>
+</html>
---
+++ SweetwaterTest/SweetwaterComments.aspx.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using MySql.Data.MySqlClient;
+using System.Web.Configuration;
+
+namespace SweetwaterTest
+{
+    public partial class SweetwaterComments : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            DisplayComments();
+            UpdateShipDate();
+        }
+
+        private void DisplayComments()
+        {
+            MySql.Data.MySqlClient.MySqlConnection cn;
+            cn = new MySql.Data.MySqlClient.MySqlConnection();
+
+            string strComments;
+
+            try
+            {
+                cn.ConnectionString = WebConfigurationManager.ConnectionStrings["strConnectionString"].ConnectionString;
+                cn.Open();
+            }
+            catch(MySql.Data.MySqlClient.MySqlException ex)
+            {
+                txtCandy.Text = "Connection Error. Please contact the web admin.";
+            }
+
+            MySqlCommand cm = new MySqlCommand();
+            cm.CommandText = "sweetwater_test";
+            cm.Connection = cn;
+            cm.CommandType = CommandType.TableDirect;
+            MySqlDataReader dr = cm.ExecuteReader();
+
+            while(dr.Read())
+            {
+                //-- Search comments for the following categories (assuming each comment only goes into one category):
+                //-- improvement: make searches case insensitive
+                strComments = dr[1].ToString();
+
+                if (strComments.Contains("candy") || strComments.Contains("CANDY") || strComments.Contains("smarties") || strComments.Contains("Taffy"))
+                    txtCandy.Text = txtCandy.Text + dr[0].ToString() + ": " + strComments + "\r\n";
+
+                else if (strComments.Contains("call") || strComments.Contains("CALL") || strComments.Contains("contact"))
+                    txtCallMe.Text = txtCallMe.Text + dr[0].ToString() + ": " + strComments + "\r\n";
+
+                else if (strComments.Contains("referred") || strComments.Contains("REFERRED") || strComments.Contains("refer"))
+                    txtReferred.Text = txtReferred.Text + dr[0].ToString() + ": " + strComments + "\r\n";
+
+                else if (strComments.Contains("signature") || strComments.Contains("SIGNATURE"))
+                    txtSignature.Text = txtSignature.Text + dr[0].ToString() + ": " + strComments + "\r\n";
+
+                else
+                    txtDisplay.Text = txtDisplay.Text + dr[0].ToString() + ": " + strComments + "\r\n";
+
+            }
+            cn.Close();
+        }
+
+        private void UpdateShipDate()
+        {
+            MySql.Data.MySqlClient.MySqlConnection cn;
+            MySql.Data.MySqlClient.MySqlConnection cnUpdate;
+
+            cn = new MySql.Data.MySqlClient.MySqlConnection();
+            cnUpdate = new MySql.Data.MySqlClient.MySqlConnection();
+
+            string strComments;
+            string strUpdate;
+            int loc;
+            string strDate;
+
+            try
+            {
+                cn.ConnectionString = WebConfigurationManager.ConnectionStrings["strConnectionString"].ConnectionString;
+                cn.Open();
+            }
+            catch (MySql.Data.MySqlClient.MySqlException ex)
+            {
+                txtCandy.Text = "Connection Error. Please contact the web admin.";
+            }
+
+            MySqlCommand cm = new MySqlCommand();
+            cm.CommandText = "sweetwater_test";
+            cm.Connection = cn;
+            cm.CommandType = CommandType.TableDirect;
+            MySqlDataReader dr = cm.ExecuteReader();
+
+
+            //-- update info
+            cnUpdate.ConnectionString = WebConfigurationManager.ConnectionStrings["strConnectionString"].ConnectionString;
+            MySqlCommand cmUpdate = new MySqlCommand();
+            cmUpdate.CommandText = "sweetwater_test";
+            cmUpdate.Connection = cnUpdate;
+
+
+            while (dr.Read())
+            {
+                strComments = dr[1].ToString();
+
+                loc = strComments.IndexOf("Expected Ship Date");
+
+                if (loc > 0)
+                {
+                    //txtTest.Text = txtTest.Text + dr[0].ToString() + ": " + loc.ToString() + "\r\n";
+                    //txtTest.Text = txtTest.Text + strComments.Substring(loc + 20);
+                    
+                    //-- assuming 8 characters for the date
+                    strDate = strComments.Substring(loc + 20, 8);
+
+                    strUpdate = "UPDATE sweetwater_test SET shipdate_expected = '" + strDate + "' WHERE orderid = " + dr[0];
+                    //txtTest.Text = txtTest.Text + strUpdate + "\r\n";
+
+                    //-- update table
+                    cmUpdate.CommandText = strUpdate;
+                    cnUpdate.Open();
+                    cmUpdate.ExecuteNonQuery();
+                    cnUpdate.Close();
+                    
+                    //txtTest.Text = txtTest.Text + "\r\n";
+                }
+
+            }
+            cn.Close();
+        }
+
+    }
+}
